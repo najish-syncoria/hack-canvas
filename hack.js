7a2d449a5a0b2cee549731b3f64d258d65184b6e
Najish Mahmud <najishmahmud@gmail.com>
2022-11-22 14:24:10 +0600

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,39 @@
+const canvas = document.querySelector("canvas");
+const recordBtn = document.querySelector("#r-button");
+
+let recording = false;
+let mediaRecorder;
+let recordedChunks;
+
+recordBtn.addEventListener("click", () => {
+  recording = !recording;
+    if(recording){
+            recordBtn.textContent = "Stop";
+            const stream = canvas.captureStream(25);
+            mediaRecorder = new MediaRecorder(stream, {
+                mimeType: 'video/webm;codecs=vp9'
+                                ignoreMutedMedia: true
+            });
+            recordedChunks = [];
+            mediaRecorder.ondataavailable = e => {
+                if(e.data.size > 0){
+                    recordedChunks.push(e.data);
+                }
+            };
+            mediaRecorder.start();
+        } else {
+            recordBtn.textContent = "Record"
+            mediaRecorder.stop();
+            setTimeout(() => {
+                const blob = new Blob(recordedChunks, {
+                    type: "video/webm"
+                });
+                const url = URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "recording.webm";
+                a.click();
+                URL.revokeObjectURL(url);
+            },0);
+        }
+});
